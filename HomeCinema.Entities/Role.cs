9742be8f38d54a589be631c526f2b7d6cab0f3bf
Vijay Kumbhoje <vijay.kumbhoje@outlook.com>
2019-06-27 22:36:38 +0530

    Changed Project

---
+++ HomeCinema.Entities/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCinema.Entities
+{
+    public class Role :IEntityBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ HomeCinema.Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCinema.Entities
+{
+   public class User : IEntityBase
+    {
+        public User()
+        {
+            UserRoles = new List<UserRoles>();
+        }
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public string Salt { get; set; }
+        public bool IsLocked { get; set; }
+        public DateTime DateCreated { get; set; }
+        public virtual ICollection<UserRoles> UserRoles { get; set; }
+    }
+}
